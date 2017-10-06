a0fe9a246f9e89783aa8f173b4606faeb3c6406b
tweddielin <tweddielin@gmail.com>
2017-10-06 16:19:29 -0500

    representativeness analysis update

--- representativeness_analysis/jobposting_ts.py
+++ representativeness_analysis/jobposting_ts.py
@@ -17,16 +17,20 @@ for y in range(1, 5):
         quarters.append('201'+str(y)+'Q'+str(q))
 
 quarters.remove('2011Q1')
-
+quarters=['2013Q1']
 for q in quarters:
-	job_postings_generator = job_postings(s3_conn, q, 'open-skills-private/job_postings_common')
-	logging.info('Counting jobposting for {}'.format(q))
-	for jp in job_postings_generator:
-		d = json.loads(jp)
-		if d['id'][:3] != 'NLX':
-			date = d['datePosted']
-			ts[date] = ts[date] + 1
+    job_postings_generator = job_postings(s3_conn, q, 'open-skills-private/job_postings_common')
+    logging.info('Counting jobposting for {}'.format(q))
+    for jp in job_postings_generator:
+        d = json.loads(jp)
+
+        if d['id'][:3] == 'NLX':
+            logging.info('Excluding NLX data')
+
+        if d['id'][:3] != 'NLX':
+            date = d['datePosted']
+            ts[date] = ts[date] + 1
 
 
-with open('jobposting_2011_2014_v1.json', 'w') as fp:
-	json.dump(ts, fp)
+with open('jobposting_2013.json', 'w') as fp:
+    json.dump(ts, fp)
